b18d0156b6ef7dc2cedefa91fba403643bdb836c
RomanZlobin <rvzlobin@gmail.com>
2022-06-02 18:08:03 +0700

    Added volume, prevStation and testes

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -4,28 +4,64 @@ public class Radio {
     private int currentStation;
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation < 0){
+        if (currentStation < 0) {
             return;
         }
-        if (currentStation >9){
+        if (currentStation > 9) {
             return;
         }
         this.currentStation = currentStation;
     }
+
     public void next() {
         if (currentStation < 9) {
             currentStation = currentStation + 1;
-        }
-        else{ currentStation = 0;
+        } else {
+            currentStation = 0;
 
         }
     }
-    public int getCurrentStation(){
+
+    public void prev() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public int getCurrentStation() {
         return currentStation;
     }
 
 
-    public int currentVolume;
+    private int currentVolume;
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
 
 
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTest {
@@ -23,6 +24,7 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
         Assertions.assertEquals(expected, actual);
     }
+
     @ParameterizedTest
     @CsvSource({
             "-1, 1",
@@ -39,6 +41,63 @@ public class RadioTest {
         radio.setCurrentStation(setCurrentStation);
         radio.next();
         int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "-1, 9",
+            "0, 9",
+            "1, 0",
+            "2, 1",
+            "8, 7",
+            "9, 8",
+            "10, 9",
+            "11, 9"
+    })
+    public void prevStation(int setCurrentStation, int expected) {
+
+        Radio radio = new Radio();
+        radio.setCurrentStation(setCurrentStation);
+        radio.prev();
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "-2, 1",
+            "-1, 1",
+            "0, 1",
+            "1, 2",
+            "8, 9",
+            "9, 10",
+            "10, 10",
+            "11, 1"
+    })
+    public void plusVolume(int setCurrentVolume, int expected) {
+
+        Radio radio = new Radio();
+        radio.setCurrentVolume(setCurrentVolume);
+        radio.increaseVolume();
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "-1, 0",
+            "0, 0",
+            "1, 0",
+            "8, 7",
+            "9, 8",
+            "10, 9",
+            "11, 0",
+            "12, 0"
+    })
+    public void minusVolume(int setCurrentVolume, int expected) {
+
+        Radio radio = new Radio();
+        radio.setCurrentVolume(setCurrentVolume);
+        radio.reduceVolume();
+        int actual = radio.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
 }
